5961ba528cff686cc7521d857a3b779c09dd66d6
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-29 22:40:32 +0200

    initial commit

---
+++ src/main/java/com/github/gabrielruiu/spring/cloud/config/redis/CloudConfigServerRedisAutoConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.gabrielruiu.spring.cloud.config.redis;
+
+/**
+ * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
+ */
+
+public class CloudConfigServerRedisAutoConfiguration {
+}
