858b06fdf03d2b2add4b50c84531cfff0aafc609
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-08-01 21:42:01 +0200

    Delete spring.factories, as it is no longer needed because the EnvironmentRepositoryConfiguration in spring-cloud-config is not an auto-configuration, and it is not possible, using the @AutoConfigureBefore annotation, to set the order of the auto-configure process

--- src/main/java/com/github/gabrielruiu/spring/cloud/config/redis/CloudConfigServerRedisAutoConfiguration.java
+++ src/main/java/com/github/gabrielruiu/spring/cloud/config/redis/CloudConfigServerRedisAutoConfiguration.java
@@ -1,12 +1,10 @@
 package com.github.gabrielruiu.spring.cloud.config.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.config.server.config.ConfigServerProperties;
-import org.springframework.cloud.config.server.config.EnvironmentRepositoryConfiguration;
 import org.springframework.cloud.config.server.environment.EnvironmentRepository;
 import org.springframework.cloud.config.server.environment.SearchPathLocator;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +17,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @ConditionalOnClass(StringRedisTemplate.class)
 @ConditionalOnMissingBean(EnvironmentRepository.class)
 @ConditionalOnProperty(value = "spring.cloud.config.server.redis.enabled", havingValue = "true")
-@AutoConfigureBefore(EnvironmentRepositoryConfiguration.class)
 @Configuration
 public class CloudConfigServerRedisAutoConfiguration {
 
