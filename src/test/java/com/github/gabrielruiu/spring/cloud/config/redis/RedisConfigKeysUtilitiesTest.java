199a96b221869c1ad2bab5f38301bc203a0c4d33
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-08-03 21:34:37 +0200

    Add test for RedisConfigKeysUtilities

---
+++ src/test/java/com/github/gabrielruiu/spring/cloud/config/redis/RedisConfigKeysUtilitiesTest.java
@@ -0,0 +1,33 @@
+package com.github.gabrielruiu.spring.cloud.config.redis;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
+ */
+public class RedisConfigKeysUtilitiesTest {
+
+    RedisConfigKeysUtilities keysUtilities = new RedisConfigKeysUtilities();
+
+    @Test
+    public void shouldReturnExpectedRedisKey() {
+        String expectedKey = "application:default:master:*";
+
+        String actualKey = keysUtilities.redisConfigKeyTemplate("application", "default", "master");
+
+        assertThat(actualKey, is(expectedKey));
+    }
+
+    @Test
+    public void shouldExtractThePropertyNameFromTheRedisKey() {
+        String expectedPropertyName = "format:date";
+
+        String actualPropertyName = keysUtilities.extractPropertyNameNameFromKey("application", "default", "master",
+                                                                            "application:default:master:format:date");
+
+        assertThat(actualPropertyName, is(expectedPropertyName));
+    }
+}
