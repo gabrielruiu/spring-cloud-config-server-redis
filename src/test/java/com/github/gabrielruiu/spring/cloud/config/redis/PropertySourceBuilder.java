817c6a703dd5ce7785c5c2495a4b6f60b138e57f
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-08-12 07:28:40 +0200

    Add builder to help prepare expected instances of PropertySource in tests It's better than doing everything (instantiating the PropertySource along with the Map) inside the test itself, which would occupy much space

---
+++ src/test/java/com/github/gabrielruiu/spring/cloud/config/redis/PropertySourceBuilder.java
@@ -0,0 +1,33 @@
+package com.github.gabrielruiu.spring.cloud.config.redis;
+
+import org.springframework.cloud.config.environment.PropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
+ */
+public class PropertySourceBuilder {
+
+    private String name;
+    private Map<String, String> propertyMap = new HashMap<>();
+
+    public static PropertySourceBuilder aPropertySource() {
+        return new PropertySourceBuilder();
+    }
+
+    public PropertySourceBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PropertySourceBuilder withProperty(String key, String value) {
+        this.propertyMap.put(key, value);
+        return this;
+    }
+
+    public PropertySource build() {
+        return new PropertySource(name, propertyMap);
+    }
+}
